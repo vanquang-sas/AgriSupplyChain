513edf6090de423181d8d323339b465f5527a403
Nong Van Quang <quang203262@gmail.com>
2026-04-27 16:10:26 +0700

    DB: Cập nhật các tham số cảnh báo bảng TONKHO

--- database/06_InsertData.sql
+++ database/06_InsertData.sql
@@ -5,6 +5,7 @@
 -- 1. THAMSO
 INSERT ALL
     INSERT INTO THAMSO(TenTS, GiaTri, MoTa) VALUES ('MIN_TONKHO', 10, 'Số lượng tồn kho tối thiểu');
+    INSERT INTO THAMSO(TenTS, GiaTri, MoTa) VALUES ('CANHBAO_HETHAN', 7, 'Số ngày trước khi hết hạn sẽ hiện cảnh báo');
     INSERT INTO THAMSO(TenTS, GiaTri, MoTa) VALUES ('MAX_TG_THANHTOAN', 24, 'Thời gian tối đa cho phép chờ thanh toán, nếu không sẽ huỷ đơn hàng');
     INSERT INTO THAMSO(TenTS, GiaTri, MoTa) VALUES ('DON_GIA_VANCHUYEN', 20000, 'Đơn giá vận chuyển 1 km');
     INSERT INTO THAMSO(TenTS, GiaTri, MoTa) VALUES ('GG_THUONG', 0.00, 'Giảm giá cho khách hàng loại thường');
