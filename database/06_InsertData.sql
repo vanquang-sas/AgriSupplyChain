5d5b096f3ea1ed810454fdf49b98f08909367388
Nong Van Quang <quang203262@gmail.com>
2026-05-04 20:36:30 +0700

    DB: Thay đổi pass của tài khoản trong demo data (2)

--- database/06_InsertData.sql
+++ database/06_InsertData.sql
@@ -15,7 +15,7 @@ SELECT * FROM dual;
 
 -- 3. TAIKHOAN
 INSERT ALL
-    INTO TAIKHOAN(Username, Password, LoaiTK, TrangThaiTK) VALUES ('quanly01', 'pa8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92ss01', 0, 1)
+    INTO TAIKHOAN(Username, Password, LoaiTK, TrangThaiTK) VALUES ('quanly01', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 0, 1)
     INTO TAIKHOAN(Username, Password, LoaiTK, TrangThaiTK) VALUES ('quanly02', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 0, 1)
 SELECT * FROM dual;
 
