638aca6c21b080133ed36d561087998658504ca6
hoanh080625 <hoanh080625@gmail.com>
2026-05-06 20:24:37 +0700

    FEAT: Xong tính năng giao hàng

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,3 +16,33 @@ public class Main {
         });
     }
 }
+
+// import util.DBConnection;
+// import java.sql.Connection;
+// import java.sql.ResultSet;
+// import java.sql.Statement;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         try {
+//             Connection con = DBConnection.getConnection();
+
+//             if (con != null) {
+//                 System.out.println("Kết nối thành công!");
+
+//                 Statement st = con.createStatement();
+//                 ResultSet rs = st.executeQuery("SELECT USER FROM dual");
+
+//                 if (rs.next()) {
+//                     System.out.println("Đang kết nối với USER: " + rs.getString(1));
+//                 }
+
+//             } else {
+//                 System.out.println("Kết nối thất bại!");
+//             }
+
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
---
+++ src/main/java/dao/GiaoHangDAO.java
@@ -0,0 +1,154 @@
+package dao;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.DonHangDTO;
+import util.DBConnection;
+
+public class GiaoHangDAO {
+
+     // 1. Lấy danh sách đơn hàng chờ giao
+    public List<DonHangDTO> getDanhSachChoGiao(){
+
+        List<DonHangDTO> list = new ArrayList<>();
+        String sql = "SELECT * FROM DONHANG WHERE TRANGTHAIDH = 'Chờ giao hàng'";
+        try (
+            Connection con = DBConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+        ){
+            while(rs.next()){
+                DonHangDTO dh = new DonHangDTO();
+                dh.setMaDH(rs.getString("MADH"));
+                dh.setMaKH(rs.getString("MAKH"));
+                dh.setMaNV(rs.getString("MANV"));
+                dh.setDiaChiGiaoHang(rs.getString("DIACHIGIAOHANG"));
+                dh.setTgDat(rs.getDate("TGDAT"));
+                dh.setTgGiaoYC(rs.getDate("TGGIAOYC"));
+                dh.setTgGiaoTT(rs.getDate("TGGIAOTT"));
+                dh.setLyDoHuy(rs.getString("LYDOHUY"));
+                dh.setPhiVanChuyen(rs.getDouble("PHIVANCHUYEN"));
+                dh.setTongTienHang(rs.getDouble("TONGTIENHANG"));
+                dh.setGiamGia(rs.getDouble("GIAMGIA"));
+                dh.setTongTien(rs.getDouble("TONGTIEN"));
+                dh.setTrangThaiDH(rs.getString("TRANGTHAIDH"));
+                dh.setTrangThaiTT(rs.getInt("TRANGTHAITT"));
+                // dh.setDanhSachSP(rs.getString("DANHSACHSP"));
+                dh.setPhuongThucTT(rs.getString("PHUONGTHUCTT"));
+
+                list.add(dh);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    // 2. Nhận đơn đi giao
+    public boolean nhanDonGiao(String maDH, String maNV) {
+        String sql = "{call SP_XACNHAN_GIAOHANG(?, ?)}";
+
+        try (
+            Connection con = DBConnection.getConnection();
+            CallableStatement cs = con.prepareCall(sql);
+        ) {
+            cs.setString(1, maDH);
+            cs.setString(2, maNV);
+
+            cs.execute();
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    // 3. Giao hàng thành công
+    public boolean giaoHangThanhCong(String MaDH, String MaNV) {
+        String sql = "{call SP_GIAOHANG_THANHCONG(?,?)}";
+
+        try (
+            Connection con = DBConnection.getConnection();
+            CallableStatement cs = con.prepareCall(sql);
+        ) {
+            cs.setString(1, MaDH);
+            cs.setString(2, MaNV);
+            cs.execute();
+
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    // 4. Giao hàng thất bại
+    public boolean giaoHangThatBai(String maDH) {
+        String sql = "UPDATE DONHANG SET TRANGTHAIDH =  'Đã huỷ' WHERE MADH = ?";
+
+        try (
+            Connection con = DBConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+        ) {
+            ps.setString(1, maDH);
+
+            return ps.executeUpdate() > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    // 5. Lịch sử giao hàng
+    public List<DonHangDTO> getLichSuGiaoHang() {
+        List<DonHangDTO> list = new ArrayList<>();
+
+        String sql = """
+            SELECT * FROM DONHANG
+            WHERE TRANGTHAIDH IN ('Hoàn thành', 'Giao thất bại')
+            ORDER BY TGDAT DESC
+        """;
+
+        try (
+            Connection con = DBConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+        ) {
+            while (rs.next()) {
+                DonHangDTO dh = new DonHangDTO();
+                dh.setMaDH(rs.getString("MADH"));
+                dh.setMaKH(rs.getString("MAKH"));
+                dh.setMaNV(rs.getString("MANV"));
+                dh.setDiaChiGiaoHang(rs.getString("DIACHIGIAOHANG"));
+                dh.setTgDat(rs.getDate("TGDAT"));
+                dh.setTgGiaoYC(rs.getDate("TGGIAOYC"));
+                dh.setTgGiaoTT(rs.getDate("TGGIAOTT"));
+                dh.setLyDoHuy(rs.getString("LYDOHUY"));
+                dh.setPhiVanChuyen(rs.getDouble("PHIVANCHUYEN"));
+                dh.setTongTienHang(rs.getDouble("TONGTIENHANG"));
+                dh.setGiamGia(rs.getDouble("GIAMGIA"));
+                dh.setTongTien(rs.getDouble("TONGTIEN"));
+                dh.setTrangThaiDH(rs.getString("TRANGTHAIDH"));
+                dh.setTrangThaiTT(rs.getInt("TRANGTHAITT"));
+                // dh.setDanhSachSP(rs.getString("DANHSACHSP"));
+                dh.setPhuongThucTT(rs.getString("PHUONGTHUCTT"));
+
+                list.add(dh);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+}
