7c7b79ca4d0c7a1e735d3be0b35a687058d3d063
Nong Van Quang <quang203262@gmail.com>
2026-05-08 21:08:31 +0700

    OTHER: Bỏ qua bước đăng nhập

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 import gui.AuthFrame;
+import gui.MainFrame;
+import util.Session;
+
 import javax.swing.UIManager;
+
+import dto.TaiKhoanDTO;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,9 +15,24 @@ public class Main {
             e.printStackTrace();
         }
 
+        // ==========================================
+        // CHẾ ĐỘ TEST: TỰ ĐỘNG ĐĂNG NHẬP
+        // ==========================================
+        TaiKhoanDTO mockUser = new TaiKhoanDTO();
+        mockUser.setUsername("admin_test"); 
+        mockUser.setLoaiTK(0); // 0: Quản lý, 1: Nhân viên, 2: Khách hàng
+        
+        Session.currentUser = mockUser;
+        Session.chucVu = "Quản lý (Admin)";
+        Session.tenNguoiDung = "Quản trị viên Test";
+
         // Khởi chạy màn hình Đăng nhập tổng
         java.awt.EventQueue.invokeLater(() -> {
-            new AuthFrame().setVisible(true);
+            // Mở thẳng MainFrame với quyền Admin
+            new MainFrame().setVisible(true);
+            
+            // Khi nào muốn deploy thật, hãy comment dòng trên và uncomment dòng dưới:
+            // new AuthFrame().setVisible(true);
         });
     }
 }
